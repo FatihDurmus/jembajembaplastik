01eb7596e7be37004328e8df1d962bca54d1121b
FatihDurmus <fatihdurmus15@yahoo.com>
2013-08-21 16:26:31 +0300

    ilk pasta

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Matrix::Application.config.secret_key_base = 'f1ec72b5befa2e345b1a236a389db6aa1e26a84df25d98ef643e9031197ed64f8208c3ab82590a5cc964b5b79e8b40ffaca9d812d5061340af0cd14e02416774'
